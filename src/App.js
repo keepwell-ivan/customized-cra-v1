c33900907247e4d5597b5762e1484599d0fee367
John Carl James Nebab <johncarlnebab@keepwell.clinic>
2021-04-24 20:29:45 +0800

    feat: add Switch Toggle component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Dropdown from 'components/Dropdown'
+import Toggle from 'components/Toggle'
 
 function App() {
 	return (
 		<div className="p-4">
-			<h1 className="text-5xl">Components</h1>
+			<h1 className="text-5xl mb-4">Components</h1>
 			<div>
-				<Dropdown />
+				<h3 className="font-semibold mb-2">Switch Toggle</h3>
+				<div className="border w-[40%] p-4 rounded">
+					<Toggle />
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Dropdown.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react'
-
-export default function Dropdown() {
-	const [enabled, setEnabled] = useState(false)
-
-	return <div tabIndex={0} role="button" onKeyDown={() => {}} onClick={() => {}}></div>
-}
