85d254ebe468ddba5300af495ccb75b4749b132a
John Carl James Nebab <johncarlnebab@keepwell.clinic>
2021-04-24 19:12:15 +0800

    chore: remove unnecessary files and add config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Dropdown from 'components/Dropdown'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="p-4">
+			<h1 className="text-5xl">Components</h1>
+			<div>
+				<Dropdown />
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,12 +3,5 @@ import { useState } from 'react'
 export default function Dropdown() {
 	const [enabled, setEnabled] = useState(false)
 
-	return (
-		<div
-			tabIndex={0}
-			role="button"
-			onKeyDown={() => {}}
-			onClick={() => {}}
-		></div>
-	)
+	return <div tabIndex={0} role="button" onKeyDown={() => {}} onClick={() => {}}></div>
 }
